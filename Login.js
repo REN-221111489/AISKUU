2eb21d41912a04fb4a8ebd4bba01fc16d956469c
REHUEL MIRROR <rehuelmirrorpertiwibrsaragi@gmail.com>
2025-10-27 13:59:11 +0700

    Buat tampilan awal mau pesan atau login

---
+++ Login.js
@@ -0,0 +1,5 @@
+// Efek sederhana untuk animasi teks berjalan (opsional tambahan)
+document.addEventListener("DOMContentLoaded", () => {
+  const movingText = document.getElementById("movingText");
+  movingText.style.animationPlayState = "running";
+});
